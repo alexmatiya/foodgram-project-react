987f28d2645d0b1ad9b6fae59abad8cd1435bd0e
Alex Matiya <matiacka@yandex.ru>
2023-05-30 09:42:40 +0300

    add app recipes, add app api

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
