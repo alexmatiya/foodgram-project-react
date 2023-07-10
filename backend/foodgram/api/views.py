7307a3748b25cb2a5b2e7b9afa674673a26674f1
Alex Matiya <matiacka@yandex.ru>
2023-07-10 08:10:37 +0300

    edit filter ingredients

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import HttpResponse, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
